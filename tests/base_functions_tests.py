c177312ccd8ab41c20713f8a8c066e80f37a9921
Ali Abbas <v2thegreat@gmail.com>
2018-07-12 08:48:53 -0400

    Renamed for better redability

--- tests/base_functions_tests.py
+++ tests/base_functions_tests.py
@@ -14,11 +14,11 @@ class base_functions_unit_test(TestCase, base_functions):
 
     def test_getFileName(self):
         """
-        Test to checks if _getFileName works
+        Test for _getFileName
 
         Test is done by running _getFileName with all the parameters that are used or are expected to be used, and this is checked with all the expected values
         """
-        from expected_values.getFileName import expected_outputs
+        from test_outputs.getFileName import expected_outputs
         possible_Index_Values = [x for x in range(0, 100)]
         possible_file_formats = ['md', 'HTML']
 
@@ -31,7 +31,7 @@ class base_functions_unit_test(TestCase, base_functions):
 
     def test_getDateTimeStamp(self):
         """
-        Test to check if _getDateTimeStamp works
+        Test for _getDateTimeStamp
 
         Test is done by
             * checking if _getDateTimeStamp returns anything at all
@@ -41,6 +41,25 @@ class base_functions_unit_test(TestCase, base_functions):
         assert self._getDateTimeStamp(addNewLine = False) != None
         assert isinstance(self._getDateTimeStamp(addNewLine = True), str)
         assert isinstance(self._getDateTimeStamp(addNewLine = False), str)
+
+
+    def test_checksForPossibleTitle(self):
+        """
+        Test for _checksForPossibleTitle
+
+        Test is done by
+            * Generating possible Titles
+            * Ensuring that all outputs are correct
+                * Checking if the titles have been confused with actual timestamps
+        """
+        from test_inputs.checksForPossibleTitle import test_inputs
+        from test_outputs.checksForPossibleTitle import expected_outputs
+
+        for pos, input in enumerate(test_inputs):
+            try:
+                assert self._checksForPossibleTitle(input) == expected_outputs[pos]
+            except AssertionError:
+                print(input, self._checksForPossibleTitle(input), expected_outputs[pos])
 
 
 if __name__ == '__main__':
